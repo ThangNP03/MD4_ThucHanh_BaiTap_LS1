d135735f52a8ae98d5a2b70fb6adb638fdfb0eea
ThangNP03 <103617145+ThangNP03@users.noreply.github.com>
2023-04-18 08:45:40 +0700

    Add files via upload

---
+++ md4_thuchanh_baitap_ss1.sql
@@ -0,0 +1,16 @@
+
+create database demoquanlysinhvien;
+use demoquanlysinhvien;
+
+
+create table class (
+ID int primary key auto_increment,
+`Name` varchar(50)
+);
+
+create table teacher (
+ID int primary key auto_increment,
+`Name` varchar(50),
+Age int,
+Country varchar(50)
+);
